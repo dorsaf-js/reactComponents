17a7d8365550dd0d5c63b33a521603f40cd2a608
dorsaf-js <boubakerdorsaf3@gmail.com>
2021-01-16 16:10:57 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
 import logo from './logo.svg';
 import './App.css';
 
+import ProfilPhoto from './componet/Profile/ProfilPhoto';
+import FullName from './componet/Profile/FullName';
+import Address from './componet/Profile/Address';
+
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <ProfilPhoto/>
+      <FullName/>
+      <Address/>
+
     </div>
   );
 }
---
+++ src/componet/Profile/ProfilPhoto.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const ProfilPhoto = () => {
+    return (
+        <div>
+            <img src="logo.jpg"/>
+
+            
+        </div>
+    )
+}
+
+export default ProfilPhoto 
